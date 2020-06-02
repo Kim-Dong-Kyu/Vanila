a3ef8b2b230534a5b0717f3f236916f8f929012d
dkkim <ekakth2458@gmail.com>
2020-06-02 23:27:25 +0900

    [upload] let, var, const

---
+++ let_var_const.js
@@ -0,0 +1,23 @@
+var name="kim-dong-kyu";
+console.log(name);
+
+var name = "dkkim";
+console.log(name);
+
+/*오류 없이 출력*/
+
+let name= "kim-dong-kyu";
+console.log(name);
+
+let name ="dkkim"
+console.log(name);
+
+const name = "kim-dong-kyu";
+console.log(name);
+
+
+const name ="dkkim";
+console.log(name);
+
+//두가지 경우 모두 에러 출력
+
