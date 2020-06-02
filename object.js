aa6707790cd8fd4c80ac8cbeb26f7b5a060b6dcc
dkkim <ekakth2458@gmail.com>
2020-06-03 00:06:15 +0900

    [upload] object

---
+++ object.js
@@ -0,0 +1,18 @@
+const DkInfo  =["dkkim", 26, true, "daejeon"];
+console.log(DkInfo);
+
+const DkInfo ={
+   name : "dkkim", 
+   age : 26, 
+   gender : 'male', 
+   isHandsome : true, 
+   favFood : [
+       {
+           namem: "chicken ",
+           fatty : true+
+       }
+   ]
+}
+
+console.log(DkInfo.name);
+console.log(DkInfo.age)
