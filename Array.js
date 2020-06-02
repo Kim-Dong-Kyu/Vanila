ddeb5075afdb573ca7cbd2bc41f6a5c06e3c2f07
dkkim <ekakth2458@gmail.com>
2020-06-02 23:24:52 +0900

    [upload] Array

---
+++ Array.js
@@ -0,0 +1,14 @@
+const mon = "monday";
+const tue = "tuesday";
+const wed = "wednesday";
+const thu = "thursday";
+const fri = "friday";
+const sat = "saturday";
+const sun = "sunday";
+
+console.log(mon, tue, wed, thu, fri, sat, sun)
+
+const daysOfWeek = ["monday", "tuesday", "wednesday", "thursday", "friday",
+    "saturday", "sunday"];
+
+console.log(daysOfWeek); 
