0b95504a754fea553182f922479a865741591f42
dkkim <ekakth2458@gmail.com>
2020-06-13 20:09:56 +0900

    [upload] todoList 페이지

--- clock/bg.js
+++ clock/bg.js
@@ -5,10 +5,7 @@ const IMG_NUMBER = 3;
 function paintImage(imgNumber){
     const image = new Image();
     image.src =`images/$(imgNumber + 1 ).jpg`;
-    
-
 }
-
 function init(){
     const randomNumber = 
     paintImage(randomNumber);
