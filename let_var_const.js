af529c82b73dd6f880ad4b0d127d04be89c2c9f2
dkkim <ekakth2458@gmail.com>
2020-07-12 13:11:50 +0900

    오류 확인

--- let_var_const.js
+++ let_var_const.js
@@ -12,11 +12,17 @@ console.log(name);
 let name ="dkkim"
 console.log(name);
 
+name = "dk";
+console.log(name);
+
 const name = "kim-dong-kyu";
 console.log(name);
 
 
 const name ="dkkim";
+console.log(name);
+
+const name="dk";
 console.log(name);
 
 //두가지 경우 모두 에러 출력
