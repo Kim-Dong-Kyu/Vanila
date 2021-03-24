5012c69049b8f3d253d4e81b8b667562b2f504a8
dkkim <ekakth2458@gmail.com>
2021-03-24 09:22:11 +0900

    프로토 타입 , 클로저

--- 기초문법/prototype.js
+++ 기초문법/prototype.js
@@ -1,8 +1,8 @@
 
-/*Js는 class 개념이 없어  기존의 객체를 복사하여 새로운 객체를 생성한다
+/*모던 자바스크립트에는 class 개념이 없어  기존의 객체를 복사하여 새로운 객체를 생성한다
   = 프로토 타입의 개념 */ 
 
-function Person(){}
+/*function Person(){}
 
 var kim = new Person();
 var Lee = new Person();
@@ -13,3 +13,28 @@ Person.prototype.getType = function(){
 
 console.log(kim.getType());
 console.log(Lee.getType());
+
+
+function Person(){
+    this.age = 27;
+    this.gender = "male";
+}
+
+var Kim = new Person();
+var Lee = new Person();
+
+console.log(Kim.age);
+console.log(Kim.gender)
+;   */
+
+function Person(){};
+
+Person.prototype.age = 27;
+Person.prototype.gender = "male";
+
+var Kim = new Person();
+var Lee = new Person();
+
+console.log(Kim.age);
+console.log(Kim.gender);
+
