5a68686d4363273d6b8de84e8a98d24957d3b741
dkkim <ekakth2458@gmail.com>
2021-04-07 09:02:02 +0900

    Array Object

---
+++ ECMAScript6/ArrayObject/from_Array.js
@@ -0,0 +1,8 @@
+let arrayObj = Array.from({0:"zero", 1: "one", length: 2});
+console.log(Array.isArray(arrayObj));
+console.log(arrayObj);
+
+let stringObj = Array.from("ABC");
+console.log(stringObj);
+
+
