0d4142f96c9da9f8f960754ef3afb1dfc6a72d7d
dkkim <ekakth2458@gmail.com>
2021-04-13 13:15:21 +0900

    template 리터럴

---
+++ ECMAScript6/templateLiteral/templateLlteral.js
@@ -0,0 +1,7 @@
+console.log("1:", `1234ABC가나다라`); 
+// expression 조합이 없기 때문에 template literal 이라고 볼 수는 없음
+console.log("2:", `라인1 \n 라인2`);
+console.log("3:", `첫 번쨰 줄 
+두 번째 줄`);
+let one = 1, two = 2; 
+console.log("4", `1+2는 ${one + two}입니다` ); /*표현식 조합*/ 
