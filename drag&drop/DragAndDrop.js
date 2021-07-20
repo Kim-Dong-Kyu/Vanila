258c7a64fd5325069f4cdc3ae08c1ad7a193a615
dkkim <ekakth2458@gmail.com>
2021-07-21 08:44:02 +0900

    Drag And Drop

--- drag&drop/DragAndDrop.js
+++ drag&drop/DragAndDrop.js
@@ -1,61 +1,67 @@
-function DragNSort (config) {
+function DragNSort(config) {
     this.$activeItem = null;
     this.$container = config.container;
-      this.$items = this.$container.querySelectorAll('.' + config.itemClass);
+    this.$items = this.$container.querySelectorAll("." + config.itemClass);
     this.dragStartClass = config.dragStartClass;
     this.dragEnterClass = config.dragEnterClass;
   }
   
   DragNSort.prototype.removeClasses = function () {
-      [].forEach.call(this.$items, function ($item) {
-          $item.classList.remove(this.dragStartClass, this.dragEnterClass);
-    }.bind(this));
+    [].forEach.call(
+      this.$items,
+      function ($item) {
+        $item.classList.remove(this.dragStartClass, this.dragEnterClass);
+      }.bind(this)
+    );
   };
   
   DragNSort.prototype.on = function (elements, eventType, handler) {
-      [].forEach.call(elements, function (element) {
-      element.addEventListener(eventType, handler.bind(element, this), false);
-    }.bind(this));
+    [].forEach.call(
+      elements,
+      function (element) {
+        element.addEventListener(eventType, handler.bind(element, this), false);
+      }.bind(this)
+    );
   };
   
   DragNSort.prototype.onDragStart = function (_this, event) {
     _this.$activeItem = this;
   
     this.classList.add(_this.dragStartClass);
-    event.dataTransfer.effectAllowed = 'move';
-    event.dataTransfer.setData('text/html', this.innerHTML);
+    event.dataTransfer.effectAllowed = "move";
+    event.dataTransfer.setData("text/html", this.innerHTML);
   };
   
   DragNSort.prototype.onDragEnd = function (_this) {
-      this.classList.remove(_this.dragStartClass);
+    this.classList.remove(_this.dragStartClass);
   };
   
   DragNSort.prototype.onDragEnter = function (_this) {
-      this.classList.add(_this.dragEnterClass);
+    this.classList.add(_this.dragEnterClass);
   };
   
   DragNSort.prototype.onDragLeave = function (_this) {
-      this.classList.remove(_this.dragEnterClass);
+    this.classList.remove(_this.dragEnterClass);
   };
   
   DragNSort.prototype.onDragOver = function (_this, event) {
     if (event.preventDefault) {
-    event.preventDefault();
+      event.preventDefault();
     }
   
-    event.dataTransfer.dropEffect = 'move';
+    event.dataTransfer.dropEffect = "move";
   
     return false;
   };
   
   DragNSort.prototype.onDrop = function (_this, event) {
-      if (event.stopPropagation) {
+    if (event.stopPropagation) {
       event.stopPropagation();
     }
   
     if (_this.$activeItem !== this) {
       _this.$activeItem.innerHTML = this.innerHTML;
-      this.innerHTML = event.dataTransfer.getData('text/html');
+      this.innerHTML = event.dataTransfer.getData("text/html");
     }
   
     _this.removeClasses();
@@ -64,23 +70,24 @@ function DragNSort (config) {
   };
   
   DragNSort.prototype.bind = function () {
-      this.on(this.$items, 'dragstart', this.onDragStart);
-      this.on(this.$items, 'dragend', this.onDragEnd);
-      this.on(this.$items, 'dragover', this.onDragOver);
-      this.on(this.$items, 'dragenter', this.onDragEnter);
-      this.on(this.$items, 'dragleave', this.onDragLeave);
-      this.on(this.$items, 'drop', this.onDrop);
+    this.on(this.$items, "dragstart", this.onDragStart);
+    this.on(this.$items, "dragend", this.onDragEnd);
+    this.on(this.$items, "dragover", this.onDragOver);
+    this.on(this.$items, "dragenter", this.onDragEnter);
+    this.on(this.$items, "dragleave", this.onDragLeave);
+    this.on(this.$items, "drop", this.onDrop);
   };
   
   DragNSort.prototype.init = function () {
-      this.bind();
+    this.bind();
   };
   
   // Instantiate
   var draggable = new DragNSort({
-      container: document.querySelector('.drag-list'),
-    itemClass: 'drag-item',
-    dragStartClass: 'drag-start',
-    dragEnterClass: 'drag-enter'
+    container: document.querySelector(".drag-list"),
+    itemClass: "drag-item",
+    dragStartClass: "drag-start",
+    dragEnterClass: "drag-enter"
   });
   draggable.init();
+  
