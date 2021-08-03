45961359a9e63b4ee7ae7064712b90595dcf35f2
dkkim <ekakth2458@gmail.com>
2021-08-04 08:45:00 +0900

    filter

--- ECMAScript6/ArrayObject/filter.js
+++ ECMAScript6/ArrayObject/filter.js
@@ -1,11 +1,13 @@
-
-var testArray = [1,2,3,4,1,2];
-
-var uniqArray = testArray.filter((element, index) => {
-    return  testArray.indexOf(element) === index
+var dupArr = [1,2,3,4,5,6,1,2,3];
+var uniqArr = dupArr.filter((element, index)=>{
+    return dupArr.indexOf(element) === index
 })
 
-console.log(uniqArray);
+console.log(uniqArr);
+
+var dupArr2 = [1,20,3,40]
+console.log(dupArr2.indexOf(40));
+ 
 
 
-var 
+
