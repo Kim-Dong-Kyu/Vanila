6c7bb294779b5135110d69e064d7f301c18d6371
dkkim <ekakth2458@gmail.com>
2021-08-05 08:59:24 +0900

    filter + spread

--- ECMAScript6/ArrayObject/filter.js
+++ ECMAScript6/ArrayObject/filter.js
@@ -1,13 +1,9 @@
-var dupArr = [1,2,3,4,5,6,1,2,3];
-var uniqArr = dupArr.filter((element, index)=>{
-    return dupArr.indexOf(element) === index
-})
+var dupArr=[1,2,3,4,5,6,7,8,9];
+var dupArr2= [...dupArr,8,9,10];
 
-console.log(uniqArr);
-
-var dupArr2 = [1,20,3,40]
-console.log(dupArr2.indexOf(40));
- 
+var uniqArr = dupArr2.filter((element, index) => {
+    return dupArr2.indexOf(element) === index;
+});
 
 
-
+console.log(uniqArr); 
