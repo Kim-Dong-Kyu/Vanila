0ad56b54e70aee7c07a474c943b26eab87ad79bf
dkkim <ekakth2458@gmail.com>
2021-08-06 09:05:48 +0900

    Visual Strudio node Test

--- ECMAScript6/ArrayObject/filter.js
+++ ECMAScript6/ArrayObject/filter.js
@@ -7,3 +7,12 @@ var uniqArr = dupArr2.filter((element, index) => {
 
 
 console.log(uniqArr); 
+
+
+var dupArr = [1,2,3,4,5,6,7,8,1,2,3];    
+var uniqArr2 = dupArr.filter((element, index) => {
+    return dupArr.indexOf(element) === index
+}) 
+
+console.log(uniqArr2);
+
